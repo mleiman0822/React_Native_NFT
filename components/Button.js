450d5c67ef94d0912b0b4dd507d0ed7af8444720
Matthew T Leiman <matthewtleiman@gmail.com>
2022-04-11 15:17:54 -0600

    Sub information for NFT cards in view

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import { TouchableOpacity, View, Text, Image } from 'react-native'
-export const CircleButton = () => {
+import { COLORS, SIZES, FONTS, SHADOWS } from '../constants'
+
+
+export const CircleButton = ({imgUrl, handlePress, ...props}) => {
   return (
-    <TouchableOpacity>
-        
+    <TouchableOpacity
+    style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: 'absolute',
+        borderRadius: SIZES.extraLarge,
+        alignItems: 'center',
+        justifyContent: 'center',
+        ...SHADOWS.light,
+        ...props,
+    }}
+    onpress={handlePress}
+    >
+        <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24}}
+        />
     </TouchableOpacity>
   )
 }
--- components/NFTCard.js
+++ components/NFTCard.js
@@ -3,6 +3,7 @@ import { useNavigation } from '@react-navigation/native'
 import React from 'react'
 import { COLORS, SIZES, SHADOWS, assets } from '../constants'
 import { CircleButton, RectButton } from './Button'
+import { SubInfo, EthPrice, NFTTitle,  } from './SubInfo'
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -27,6 +28,20 @@ const NFTCard = ({ data }) => {
         }}
         />
         <CircleButton imgUrl={assets.heart} right={10} top={10}/>
+      </View>
+
+      <SubInfo/>
+
+      <View style={{ width: '100%', padding: SIZES.font}}>
+        <NFTTitle 
+        title={data.name}
+        subTitle={data.creator}
+        titleSize={SIZES.large}
+        subTitleSize={SIZES.small}
+        />
+      </View>
+      <View>
+        
       </View>
     </View>
   )
