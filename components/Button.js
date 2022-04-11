c185bbc4e6f1b2640ac60d1f3d8bad64d2898d5b
Matthew T Leiman <matthewtleiman@gmail.com>
2022-04-11 14:55:21 -0600

    Inital commit (After a bunch of stuff)

---
+++ components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { TouchableOpacity, View, Text, Image } from 'react-native'
+export const CircleButton = () => {
+  return (
+    <TouchableOpacity>
+        
+    </TouchableOpacity>
+  )
+}
+
+export const RectButton = () => {
+    return (
+      <View>
+        <Text>CircleButton</Text>
+      </View>
+    )
+  }
+
