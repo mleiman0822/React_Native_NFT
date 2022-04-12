06fead139df0bc5d9bf5e1d8da97f3f61fee14a4
Matthew T Leiman <matthewtleiman@gmail.com>
2022-04-12 11:43:23 -0600

    Details/bid/search functionality

--- components/Button.js
+++ components/Button.js
@@ -1,54 +1,55 @@
-import React from 'react'
-import { TouchableOpacity, View, Text, Image } from 'react-native'
-import { COLORS, SIZES, FONTS, SHADOWS } from '../constants'
+import React from "react";
+import { TouchableOpacity, Text, Image } from "react-native";
 
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
 
-export const CircleButton = ({imgUrl, handlePress, ...props}) => {
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
     <TouchableOpacity
-    style={{
+      style={{
         width: 40,
         height: 40,
         backgroundColor: COLORS.white,
-        position: 'absolute',
+        position: "absolute",
         borderRadius: SIZES.extraLarge,
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
         ...SHADOWS.light,
         ...props,
-    }}
-    onpress={handlePress}
+      }}
+      onPress={handlePress}
     >
-        <Image
+      <Image
         source={imgUrl}
         resizeMode="contain"
-        style={{ width: 24, height: 24}}
-        />
+        style={{ width: 24, height: 24 }}
+      />
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
-    return (
-      <TouchableOpacity
+  return (
+    <TouchableOpacity
       style={{
-          backgroundColor: COLORS.primary,
-          borderRadius: SIZES.extraLarge,
-          minWidth: minWidth,
-          padding: SIZES.small,
-          ...props,
+        backgroundColor: COLORS.primary,
+        padding: SIZES.small,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        ...props,
       }}
-      onpress={handlePress}
-      >
-        <Text style={{
+      onPress={handlePress}
+    >
+      <Text
+        style={{
           fontFamily: FONTS.semiBold,
           fontSize: fontSize,
           color: COLORS.white,
-          textAlign: 'center',
-        }}>
-          Place a bid
-        </Text>
-      </TouchableOpacity>
-    )
-  }
-
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
+  );
+};
